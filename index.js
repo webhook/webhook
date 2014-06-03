f772c11189c04ca3bf352a2e5677222d66a8d615
Ian Kelly <ltsquigs@gmail.com>
2014-06-03 11:22:17 -0700

    initial commit of instal files

---
+++ index.js
@@ -0,0 +1,70 @@
+'use strict';
+
+require('colors');
+var program = require('commander');
+
+module.exports = function (argv) {
+
+  require('./lib/notifier.js')('v10', function() {
+    program
+      .version(require('./package.json').version);
+
+    program
+      .command('create <siteName>')
+      .description('Create a new webhook site')
+      .action(function (siteName) {
+        require('./lib/create.js')({
+          siteName: siteName.toLowerCase()
+        });
+      });
+
+    program
+      .command('init <siteName>')
+      .description('Initializes a site with configuration files')
+      .action(function (siteName) {
+        require('./lib/init.js')({
+          siteName: siteName.toLowerCase()
+        });
+      });
+
+    program
+      .command('preset-build')
+      .description('Generates a preset-data.json file from a webhook directory')
+      .action(function () {
+        require('./lib/preset-build.js')();
+      });
+
+    program
+      .command('update')
+      .description('Updates a webhook site with the latest generate code')
+      .action(function () {
+        require('./lib/update.js')();
+      });
+
+    program
+      .command('push')
+      .description('Push webhook directory to server')
+      .action(function () {
+        require('./lib/push.js')();
+      });
+      
+    program
+      .command('deploy')
+      .description('Push webhook directory to server')
+      .action(function () {
+        require('./lib/push.js')();
+      });
+
+    program
+      .command('serve')
+      .description('Serves a webhook site locally')
+      .action(function () {
+        require('./lib/serve.js')();
+      });
+    
+    program
+      .parse(argv);
+
+    if (!program.args.length) program.help();
+  });
+};
