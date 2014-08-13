bfced21dd06637ebd9b971424583446b0cb72dda
Ian Kelly <ltsquigs@gmail.com>
2014-08-13 11:48:14 -0700

    allow custom firebase/server

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ module.exports = function (argv) {
 
   require('./lib/notifier.js')('v14', function() {
     program
-      .version(require('./package.json').version);
+      .version(require('./package.json').version)
+      .option('-f', '--firebase [firebasename]', 'Use the specified firebase instead of webhook main, for self hosting mode')
+      .option('-s', '--server [uploadserver]', 'Use this server when uploading files, for self hosting mode');
+
 
     program
       .command('create <siteName>')
       .description('Create a new webhook site')
       .action(function (siteName) {
         require('./lib/create.js')({
-          siteName: siteName.toLowerCase()
+          siteName: siteName.toLowerCase(),
+          firebase: program.firebase
         });
       });
 
@@ -23,7 +27,8 @@ module.exports = function (argv) {
       .description('Initializes a site with configuration files')
       .action(function (siteName) {
         require('./lib/init.js')({
-          siteName: siteName.toLowerCase()
+          siteName: siteName.toLowerCase(),
+          firebase: program.firebase
         });
       });
 
@@ -31,42 +36,56 @@ module.exports = function (argv) {
       .command('list-sites')
       .description('Lists all the sites that the user is an owner/user on')
       .action(function () {
-        require('./lib/list-sites.js')();
+        require('./lib/list-sites.js')({
+          firebase: program.firebase
+        });
       });
 
     program
       .command('preset-build')
       .description('Generates a preset-data.json file from a webhook directory')
       .action(function () {
-        require('./lib/preset-build.js')(false);
+        require('./lib/preset-build.js')(false, {
+          firebase: program.firebase
+        });
       });
 
     program
       .command('preset-build-all')
       .description('Generates a preset-data.json file from a webhook directory which includes data')
       .action(function () {
-        require('./lib/preset-build.js')(true);
+        require('./lib/preset-build.js')(true, {
+          firebase: program.firebase
+        });
       });
 
     program
       .command('update')
       .description('Updates a webhook site with the latest generate code')
       .action(function () {
-        require('./lib/update.js')();
+        require('./lib/update.js')({,
+          firebase: program.firebase
+        });
       });
 
     program
       .command('push')
       .description('Push webhook directory to server')
       .action(function () {
-        require('./lib/push.js')();
+        require('./lib/push.js')({
+          firebase: program.firebase,
+          server: server
+        });
       });
       
     program
       .command('deploy')
       .description('Push webhook directory to server')
       .action(function () {
-        require('./lib/push.js')();
+        require('./lib/push.js')({
+          firebase: program.firebase,
+          server: server
+        });
       });
 
     program
@@ -74,7 +93,8 @@ module.exports = function (argv) {
       .description('Serves a webhook site locally')
       .action(function (port) {
         require('./lib/serve.js')({
-          port: port || null
+          port: port || null,
+          firebase: program.firebase
         });
       });
     
