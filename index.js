744d2b893f9208ea4bbfb180fbe27ed6519bbdc8
Ian Kelly <ltsquigs@gmail.com>
2014-10-08 11:04:32 -0700

    update wh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 require('colors');
 var program = require('commander');
 
-var version = 'v15';
+var version = 'v16';
 
 module.exports = function (argv) {
   require('./lib/notifier.js')(version, function() {
