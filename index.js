6b4319d93e59f960702f2f10d56143d4bedaff7f
Ian Kelly <ltsquigs@gmail.com>
2014-11-21 15:14:20 -0800

    major eversion

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 require('colors');
 var program = require('commander');
 
-var version = 'v21';
+var version = 'v22';
 
 module.exports = function (argv) {
   require('./lib/notifier.js')(version, function() {
